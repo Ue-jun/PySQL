497e4beeb61f2f639a67f4d9b0586dff5dca883e
Ue-jun <jussan.suibu@gmail.com>
2024-02-22 14:28:03 +0900

    FLASK有効化

--- PySQL.py
+++ PySQL.py
@@ -1,35 +1,42 @@
 import pymysql.cursors
 import configparser
+from flask import Flask, render_template
 
-# 設定ファイルのパス
-config_file = '../../settings.ini'
+app = Flask(__name__)
 
-# コンフィグパーサーの初期化
-config = configparser.ConfigParser()
-config.read(config_file)
+@app.route('/')
+def init():
+    # 設定ファイルのパス
+    config_file = '../../settings.ini'
 
-# コンフィグのルート
-db_config = config['LOCAL_DB']
+    # コンフィグパーサーの初期化
+    config = configparser.ConfigParser()
+    config.read(config_file)
 
-# データベース接続情報（開発用）
-connection = pymysql.connect(host=db_config['host'],
-                             user=db_config['user'],
-                             password=db_config['password'],
-                             database=db_config['database'],
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+    # コンフィグのルート
+    db_config = config['LOCAL_DB']
 
-try:
-    with connection.cursor() as cursor:
-        # SQL実行
-        sql = 'SELECT * FROM 楽天商品管理番号マスタ'
-        cursor.execute(sql)
+    # データベース接続情報（開発用）
+    connection = pymysql.connect(host=db_config['host'],
+                                user=db_config['user'],
+                                password=db_config['password'],
+                                database=db_config['database'],
+                                charset='utf8mb4',
+                                cursorclass=pymysql.cursors.DictCursor)
 
-        # 結果の取得
-        result = cursor.fetchall()
-        for row in result:
-            print(row['Title'])
+    try:
+        with connection.cursor() as cursor:
+            # SQL実行
+            sql = 'SELECT * FROM 楽天商品管理番号マスタ'
+            cursor.execute(sql)
 
-finally:
-    connection.close()
+            # 結果の取得
+            results = cursor.fetchall()
+
+    finally:
+        connection.close()
+        return render_template('index.html', results=results)
+
+if __name__ == '__main__' :
+    app.run(debug=True)
 
